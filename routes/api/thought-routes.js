f714d94524057691ac37413e375abd9fa2c77739
ryanmuhl <ryanmuhl@hotmail.com>
2022-06-12 21:08:29 -0400

    update folder structure

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -16,15 +16,22 @@ const {
 
 //GET all Thoughts, Create Thought (get and post)
 //api/thoughts
-router.route('/').get(getAllThoughts).post(createThoughts);
+router.route('/')
+    .get(getAllThoughts)
+    .post(createThoughts);
 
 //DELETE Thought by ID, EDIT Thought by ID (delete)
 //api/thoughts/thoughtId
-router.route('/:thoughtId').put(updateThought).delete(deleteThought).get(getThoughtById);
+router.route('/:thoughtId')
+    .put(updateThought)
+    .delete(deleteThought)
+    .get(getThoughtById);
 
 //Post Reaction to Thoughts by ID, Delete Reaction from Thought by ID (POST DELETE)
 //api/thoughts/thoughtId/reactions
-router.route('/:thoughtId/reactions').post(createReaction).delete(deleteReaction);
+router.route('/:thoughtId/reactions')
+    .post(createReaction)
+    .delete(deleteReaction);
 
 // Export module router
 module.exports = router;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -15,14 +15,21 @@ const {
 
 //GET all Users and Create User api/users (GET POST)
 //api/user
-router.route('/').get(getAllUsers).post(createUsers)
+router.route('/')
+    .get(getAllUsers)
+    .post(createUsers)
 
 //Get User by ID, Edit User by ID, Delete User by ID(GET PUT DELETE)
 //api/user/userid
-router.route('/:userId').get(getUsersById).put(updateUser).delete(deleteUser);
+router.route('/:userId')
+    .get(getUsersById)
+    .put(updateUser)
+    .delete(deleteUser);
 
 //Post Freind to User by ID, Delete Friend from User by ID (POST DELETE)
 //api/user/userId/friends/freindId
-router.route('/:userId/friends/:friendId').post(createFriend).delete(deleteFriend);
+router.route('/:userId/friends/:friendId')
+    .post(createFriend)
+    .delete(deleteFriend);
 
 module.exports = router;
